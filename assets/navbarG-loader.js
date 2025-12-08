13e7fa1123ad963ee7f6ba335e9fc884c053d7c0
Troyever <houchengl@student.unimelb.edu.au>
2025-12-08 22:21:48 +0800

    Update navbarG-loader.js

--- assets/navbarG-loader.js
+++ assets/navbarG-loader.js
@@ -1,12 +1,60 @@
-function loadNavbar() {
-  fetch("/intermediate-microeconomics-project/assets/navbarG.html")
-    .then(response => response.text())
-    .then(html => {
-      const container = document.getElementById("navbar-container");
-      if (container) container.innerHTML = html;
-    document.dispatchEvent(new Event("navbar-loaded"));
-})
-    .catch(err => console.error("Navbar load error:", err));
-}
+const NAVBAR_HTML=`
+<nav class="navbar">
+  <div class="nav-left">
+    <a href="/intermediate-microeconomics-project/index.html">Home</a>
+  </div>
 
-document.addEventListener("DOMContentLoaded", loadNavbar);
+  <div class="nav-menu">
+    <div class="dropdown">
+      <span class="dropdown-btn">Concepts ▾</span>
+      <div class="dropdown-content">
+        <a href="/intermediate-microeconomics-project/week1/concepts.html">Week 1</a>
+        <a href="/intermediate-microeconomics-project/week2/concepts.html">Week 2</a>
+        <a href="/intermediate-microeconomics-project/week3/concepts.html">Week 3</a>
+        <a href="/intermediate-microeconomics-project/week4/concepts.html">Week 4</a>
+        <a href="/intermediate-microeconomics-project/week5/concepts.html">Week 5</a>
+        <a href="/intermediate-microeconomics-project/week6/concepts.html">Week 6</a>
+        <a href="/intermediate-microeconomics-project/week7/concepts.html">Week 7</a>
+        <a href="/intermediate-microeconomics-project/week8/concepts.html">Week 8</a>
+        <a href="/intermediate-microeconomics-project/week9/concepts.html">Week 9</a>
+        <a href="/intermediate-microeconomics-project/week10/concepts.html">Week 10</a>
+        <a href="/intermediate-microeconomics-project/week11/concepts.html">Week 11</a>
+        <a href="/intermediate-microeconomics-project/week12/concepts.html">Week 12</a>
+        <!-- Add later -->
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <span class="dropdown-btn">Exercises ▾</span>
+      <div class="dropdown-content">
+        <a href="/intermediate-microeconomics-project/week1/exercise.html">Week 1</a>
+        <a href="/intermediate-microeconomics-project/week2/exercise.html">Week 2</a>
+        <a href="/intermediate-microeconomics-project/week3/exercise.html">Week 3</a>
+        <a href="/intermediate-microeconomics-project/week4/exercise.html">Week 4</a>
+        <a href="/intermediate-microeconomics-project/week5/exercise.html">Week 5</a>
+        <a href="/intermediate-microeconomics-project/week6/exercise.html">Week 6</a>
+        <a href="/intermediate-microeconomics-project/week7/exercise.html">Week 7</a>
+        <a href="/intermediate-microeconomics-project/week8/exercise.html">Week 8</a>
+        <a href="/intermediate-microeconomics-project/week9/exercise.html">Week 9</a>
+        <a href="/intermediate-microeconomics-project/week10/exercise.html">Week 10</a>
+        <a href="/intermediate-microeconomics-project/week11/exercise.html">Week 11</a>
+        <a href="/intermediate-microeconomics-project/week12/exercise.html">Week 12</a>
+      </div>
+    </div>
+     <div class="nav-menu">
+    <button id="export-desmos-btn" class="export-btn">
+      Export graph
+    </button>
+  </div>
+  </div>
+</nav>`;
+
+
+(function loadNavbar() {
+  const container = document.getElementById("navbar-container");
+  if (!container) return;
+
+  container.innerHTML = NAVBAR_HTML;
+
+  document.dispatchEvent(new Event("navbar-loaded"));
+})();
